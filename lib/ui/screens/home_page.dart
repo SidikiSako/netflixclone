20b345fa83442979510766ae1052aed5d8d4d32d
SidikiSako <sidiksako@gmail.com>
2021-11-25 15:37:05 +0100

    homePage design with colored container

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_clone/utils/constant.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,15 +12,105 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: kBackgroundColor,
-      child: Center(
-        child: Text(
-          'HOME PAGE',
-          style: TextStyle(
-            color: Colors.white,
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.red,
+            height: 500,
           ),
-        ),
+          const SizedBox(height: 15),
+          Text(
+            'Tendances actuelles',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.yellow,
+                  ),
+                  width: 110,
+                  child: Center(
+                    child: Text(index.toString()),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            'Les séries qui cartonnent',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 320,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.blue,
+                  ),
+                  width: 160,
+                  child: Center(
+                    child: Text(index.toString()),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            'Actuellement au cinéma',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.green,
+                  ),
+                  width: 110,
+                  child: Center(
+                    child: Text(index.toString()),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
