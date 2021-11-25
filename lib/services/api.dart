ff677f87bc4626bc79155c797b78e768ca138ea1
SidikiSako <sidiksako@gmail.com>
2021-11-25 16:11:29 +0100

    api.dart ajouté au projet

---
+++ lib/services/api.dart
@@ -0,0 +1,7 @@
+import 'package:netflix_clone/services/api_key.dart';
+
+class API {
+  final String apiKey = APIKey.apikey;
+  final String baseURL = 'https://api.themoviedb.org/3';
+  final String baseImageURL = 'https://image.tmdb.org/t/p/w500/';
+}
